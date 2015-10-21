17054e4aab513052da8144158cac42daa5db35eb
n0ts <naoya.n@gmail.com>
2015-10-21 20:23:10 +0900

    Missing merge

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,10 +10,6 @@ class brewcask (
 
   homebrew::tap { 'caskroom/cask': }
 
-  package { 'brew-cask':
-    require => Homebrew_Tap['caskroom/cask']
-  }
-
   file { $cask_home:
     ensure => directory
   }
