ad0268a571a3b2ab9c23d0acadc4c0e96475f823
Naoya Nakazawa <naoya.n@gmail.com>
2015-10-29 02:59:13 +0900

    Fix a test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ fixture_path = File.expand_path File.join(__FILE__, "..", "fixtures")
 RSpec.configure do |c|
   c.manifest_dir = File.join(fixture_path, "manifests")
   c.module_path  = File.join(fixture_path, "modules")
-  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
+  c.hiera_config = File.join(fixture_path, "hiera/hiera.yaml")
 end
 
 def default_test_facts
